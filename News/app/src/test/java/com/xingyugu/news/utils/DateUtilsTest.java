04b10aba284f970868cccc10d9421158942927c7
Xingyu Gu <xingyuguseu@gmail.com>
2017-05-20 02:13:47 -0700

    add PowerMock test

--- News/app/src/test/java/com/xingyugu/news/utils/DateUtilsTest.java
+++ News/app/src/test/java/com/xingyugu/news/utils/DateUtilsTest.java
@@ -1,15 +1,27 @@
 package com.xingyugu.news.utils;
+import com.google.firebase.crash.FirebaseCrash;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.text.ParseException;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by XingyuGU on 5/20/17.
  */
-
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FirebaseCrash.class})
 public class DateUtilsTest {
     String correctInputDate1 = "2016-07-25T09:56:27Z";
     String correctOutputDate1 = "Mon, 25 Jul 2016 09:56";
+
+    String incorrectInputDate1 = "2016-07-25T09";
 
     @Test
     public void formatNewsApiDate_correctDate_outputCorrectDate(){
@@ -21,5 +33,16 @@ public class DateUtilsTest {
     public void formatNewsApiDate_nullInput_outPutsNull(){
         String outputDate = DateUtils.formatNewsApiDate(null);
         assertEquals(outputDate, null);
+    }
+
+    @Test
+    public void formatNewsApiDate_incorrectInput_returnsSame(){
+        PowerMockito.mockStatic(FirebaseCrash.class);
+
+        String outputDate = DateUtils.formatNewsApiDate(incorrectInputDate1);
+
+        assertEquals(outputDate, incorrectInputDate1);
+        PowerMockito.verifyStatic();
+        FirebaseCrash.report(Matchers.isA(ParseException.class));
     }
 }
